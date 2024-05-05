babeb9a90942188f1d0b02ff5b15671437022f59
Patrick Selin <patrick.selin@gmail.com>
2024-05-05 10:55:40 +0300

    change app, check if updating 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World, updated or not!</h1>");
+  res.send("<h1>Hello World, updated or not??</h1>");
 });
 
 const PORT = 8080;
